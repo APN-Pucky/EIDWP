482e7ce840f93ad5234d435143dec0ce55f1440d
APN-Pucky <APN-Pucky@users.noreply.github.com>
2018-04-19 17:59:40 +0200

    Ü1

---
+++ Übung1/guten_tag.f90
@@ -0,0 +1,6 @@
+PROGRAM guten_tag
+IMPLICIT NONE
+
+WRITE(*,*) "Guten Tag"
+
+END PROGRAM guten_tag
---
+++ Übung1/square.f90
@@ -0,0 +1,10 @@
+PROGRAM square
+IMPLICIT NONE
+INTEGER(8) :: i
+WRITE(*,*) "x="
+READ(*,*) i
+WRITE(*,*) "x^2=" ,i**2 
+
+END PROGRAM square
+
+
