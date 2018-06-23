66b3b0e725f91c4b5c089bbf7c1d355c13c1363c
APN-Pucky <APN-Pucky@users.noreply.github.com>
2018-06-23 13:07:15 +0200

    FunktionFun2

--- Übung7/funktion.f90
+++ Übung7/funktion.f90
@@ -1,43 +1,203 @@
 MODULE funktion
         IMPLICIT NONE
-        type ff
-                type(ff), pointer :: a
-                procedure(func), pointer :: m
+        type funktion
+                procedure(func), pointer,nopass :: p_f => id
+                real :: scal=1
+                procedure(rfunc), pointer :: p_eval => base_func
+                procedure(rafunc), pointer :: p_eval_a => NULL()
+                procedure(fffunc), pointer :: p_div => base_div
+                procedure(fffunc), pointer :: p_minus => base_minus
+                procedure(fffunc), pointer :: p_plus => base_plus
+                procedure(fffunc), pointer :: p_pow => base_pow
+                procedure(fffunc), pointer :: p_mult => base_mult
+                type(funktion), pointer, dimension(:) :: p_f_array
+                logical :: is_allocated = .false.
                 CONTAINS
-                procedure, pass :: v
-                procedure, pass :: df
-                procedure, pass :: ndf
+                generic :: get => evaluate,evaluate_a
+                procedure, pass :: dealloc
+                procedure, pass :: base_func
+                procedure, pass :: evaluate
+                procedure, pass :: evaluate_a
         end type
-        abstract interface
-                function func(self,x)
-                        import ff
-                        class(ff) :: self
-                        real :: x
-                        real :: func
-                end function 
+        interface nst
+                module procedure nst_f,nst_fdf
         end interface
-      
-        CONTAINS 
-                real recursive function v(self,x)
-                        class(ff) :: self
+        abstract interface
+                real function func(x)
                         real :: x
-                        v = self%m(x)
+                end function 
+                real function rfunc(rf, x)
+                        import funktion
+                        class(funktion) :: rf
+                        real :: x
                 end function
-                function df(self)
-                        class(ff) :: self
-                        type(ff) df
-                        df%a = self 
-                        df%m => ndf
+                real function rafunc(rf, x)
+                        import funktion
+                        class(funktion) :: rf
+                        real,dimension(:) :: x
                 end function
-                real function ndf(self,x)
-                        class(ff) :: self
-                        real :: x,h=sqrt(1e-7)
-                        ndf = (self%a%v(x+h)-self%a%v(x))/h
-                end function
-                !!
 
+                function fffunc(f1,f2)
+                        import funktion
+                        class(funktion) :: f1
+                        type(funktion) :: f2,fffunc
+                end function
+        end interface
+
+        interface operator(*)
+                module procedure mult_f, mult_real_l, mult_real_r
+        end interface
+        interface operator(/)
+                module procedure div_f, div_real_l, div_real_r
+        end interface
+        interface operator(**)
+                module procedure pow_f, pow_real_l, pow_real_r
+        end interface
+        interface operator(+)
+                module procedure plus_f, plus_real_l, plus_real_r
+        end interface
+        interface operator(-)
+                module procedure minus_f, minus_real_l, minus_real_r
+        end interface
+
+
+        type(funktion) :: x,dx,d
+        real :: e = exp(1.)
+        real :: h = sqrt(1e-7)
+        real :: eps = 1e-5
+        integer :: N = 5 !odd!
+        integer :: max_iter = 10000
+
+        CONTAINS 
+                include 'base_operations.inc'
+                include 'dx_operations.inc'
+
+                subroutine alloc(self,n)
+                        type(funktion) :: self
+                        integer :: n
+                        allocate(self%p_f_array(n))
+                        self%is_allocated = .true.
+                end subroutine
+                recursive subroutine dealloc(self)
+                        class(funktion) :: self
+                        integer n,i
+                        if(.not. self%is_allocated) return
+                        n = size(self%p_f_array)
+                        DO i=1,n
+                                CALL self%p_f_array(i)%dealloc
+                        end do
+                        deallocate(self%p_f_array)
+                        self%is_allocated = .false.
+                end subroutine
+                        
+                function create_funktion(f)
+                        type(funktion) :: create_funktion
+                        procedure(func), pointer :: f
+                        !create_funktion%p_eval => base_func
+                        create_funktion%p_f => f
+                end function
+                real function base_func(self,x)
+                        real :: x
+                        class(funktion) :: self
+                        base_func = self%p_f(x)
+                end function
+
+                real function nst_fdf(f,df,x) 
+                        real :: x
+                        type(funktion) :: f,df,t
+                        integer :: i
+                        nst_fdf = x
+                        t = f/(df)
+                        DO i=1,max_iter
+                                if(abs(f%get(nst_fdf))<eps)return
+                                nst_fdf = nst_fdf -t%get(nst_fdf)
+                        end do
+                        nst_fdf = 0
+                        nst_fdf = log(nst_fdf) !ERROR No NST!
+                end function
+
+                real function nst_f(f,x)
+                        real :: x
+                        type(funktion) :: f
+                        nst_f = nst(f,f/dx,x)
+                end function
+
+                real function minimum(f,x1,x2)
+                        !Bisection => local min
+                        real :: x1,x2,bx1,bx2, fx1,fx2,t=0.
+                        real :: fm
+                        type(funktion) :: f
+                        integer :: i
+                        bx1 = x1 !avoid pointer issues
+                        bx2 = x2
+                        fm = -log(t)
+                        DO i=1,max_iter
+                                fx1 = f%get(bx1)
+                                fx2 = f%get(bx2)
+                                if(fx1<fm) then 
+                                        minimum = bx1
+                                        fm = fx1
+                                end if
+                                if(fx2<fm) then
+                                        minimum = bx2
+                                        fm = fx2
+                                end if
+                                if(fx2<fx1) then
+                                        t = (bx1+bx2)/2.
+                                        if(abs(bx1-t) < eps .OR. abs(bx2-t) <eps) return
+                                        bx1=t
+                                else
+                                        t = (bx1+bx2)/2.
+                                        if(abs(bx1-t) < eps .OR. abs(bx2-t) <eps) return
+                                        bx2=t
+                                end if
+                        end do
+                end function
+
+                real recursive function evaluate(self,x)
+                        real :: x
+                        class(funktion) :: self
+                        !n=size(self%p_f_array)
+                        evaluate = self%scal * self%p_eval(x)
+                end function
+
+                function evaluate_a(self,x)
+                        real, dimension(:) :: x
+                        real :: evaluate_a
+                        class(funktion) :: self
+                        !n=size(self%p_f_array)
+                        evaluate_a = self%scal * self%p_eval_a(x)
+                end function
+
+                function init_dx()
+                        type(funktion) :: init_dx
+                        procedure(func), pointer :: p_id => id
+                        init_dx%p_f =>  p_id !dx alone?!?
+                        init_dx%p_div => dx_div
+                        init_dx%p_mult => dx_mult
+                end function        
+                
+                function init_x()
+                        type(funktion) :: init_x
+                        procedure(func), pointer :: p_id => id
+                        init_x = create_funktion(p_id)
+                end function
+                function init_d()
+                        type(funktion) :: init_d
+                        procedure(func), pointer :: p_d => idd
+                        init_d = create_funktion(p_d)
+                end function
+                real function idd(x)
+                        real ::x
+                        idd =1.0
+                end function
                 real function id(x)
                         real :: x
-                        id = x
+                        id =x
                 end function
+                subroutine init()
+                        x = init_x()
+                        d = init_d()
+                        dx = init_dx()
+                end subroutine
 END MODULE
---
+++ Übung7/rechnung.f90
@@ -0,0 +1,85 @@
+program rechnung
+      use funktion
+      implicit none
+      real :: x_input=0 !value needed, because of memory poition should be allocated early
+      real :: best_h, h_opt_calc = sqrt(1e-7),xo,xu
+      real,dimension(3) :: ha = (/0.01,0.001,0.0001/)
+      integer :: i,s
+      procedure(func), pointer  :: p_opt_h => optimize_h
+      type(funktion) :: x2,df,opt_h,ex
+      CALL init
+      s = size(ha)
+
+      WRITE(*,100) "Ableiten"
+      x2 = (x**2.)/dx !x**2 ableiten
+      x_input = read_x("x =")
+      df = x2-2.*x !differenz funktion!
+      WRITE(*,100) "Vorwaertsdifferenz-Methode"
+      DO i=1,s
+                CALL print_h(ha(i))
+      end do
+      h = 1e-3
+      opt_h = create_funktion(p_opt_h)
+      eps = 1e-5
+      best_h = minimum(opt_h,1e-3,1e-9)
+      h = best_h
+      WRITE(*,100) "optimize h"
+      WRITE(*,101) "Bisektion"
+      CALL print_h( best_h)
+      WRITE(*,101) "Theorie"
+      CALL print_h( h_opt_calc)
+
+      WRITE(*,100) "Integrieren"
+      ex = (e**x)*dx   !e**x integrieren
+      xo = read_x( "Integral obere Grenze x1=")
+      xu = read_x( "Integral unter Grenze x2=")
+      WRITE(*,100) "Simpson-Methode"
+      N=5
+      WRITE(*,*) "int e**x von x2 bis x1 =", ex%get((/xo,xu/))
+      !WRITE(*,*) "x=", r
+      !f = (e**x)*dx
+      !r = f%get((/0.1,0./))
+      !write(*,*) r
+      !CALL x2%dealloc
+      100 FORMAT(50("="),T8,A)
+      101 FORMAT(50("-"),T5,A)
+
+      CALL df%dealloc
+      CALL x2%dealloc
+      CALL ex%dealloc
+      CALL opt_h%dealloc
+      CONTAINS 
+              subroutine print_h (param_h)
+                      real :: param_h
+                      h = param_h
+                      WRITE(*,*) "f(x)=(d*x**2)/dx , h=", param_h
+                      WRITE(*,'(T9, "|->")',advance='no') 
+                      WRITE(*,*) 'f(x)=', x2%get(x_input)
+                      WRITE(*,'(T9, "`->")',advance='no') 
+                      WRITE(*,*) 'diff=', df%get(x_input)
+              end subroutine 
+
+              function read_x (s)
+                      real :: read_x
+                      character(*) :: s
+                      WRITE(*,*) s
+                      READ(*,*) read_x
+                      WRITE(*,*) s,read_x
+              end function
+
+
+              real function optimize_h(oh)
+                      real :: oh
+                      type(funktion)  :: dff
+                      real :: x_val
+                      x_val = x_input !avoid memory/pointer issue
+                      h=oh
+                      dff = (x**2./dx)-2.*x
+                      !WRITE(*,*) oh, dff%get(2.)
+                      optimize_h = abs(dff%get(x_val))
+              end function
+
+end program
+
+
+
